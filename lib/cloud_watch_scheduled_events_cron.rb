a978375e73682bb010048548780dde75239ca8e5
abetomo <abe@enzou.tokyo>
2018-05-28 15:24:37 +0900

    Add files generated by gem command

---
+++ lib/cloud_watch_scheduled_events_cron.rb
@@ -0,0 +1,5 @@
+require "cloud_watch_scheduled_events_cron/version"
+
+module CloudWatchScheduledEventsCron
+  # Your code goes here...
+end
---
+++ lib/cloud_watch_scheduled_events_cron/version.rb
@@ -0,0 +1,3 @@
+module CloudWatchScheduledEventsCron
+  VERSION = "0.1.0"
+end
